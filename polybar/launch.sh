520e8bf5246251c9d997907d4ec900d290d832eb
AA-HAMZA <ahmed.rk30@gmail.com>
2020-12-15 01:04:53 +0200

    Updated

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,7 +4,7 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
 # Launch bar
 polybar main &
